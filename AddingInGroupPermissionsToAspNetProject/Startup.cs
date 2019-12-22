de8bc5510a47a8a3ec38ff6804e1b6ce4731e4d0
Frank Thomas <frank@thethomasproject.com>
2019-12-22 14:22:32 -0700

    Initial project creation with no updates or changes

---
+++ AddingInGroupPermissionsToAspNetProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AddingInGroupPermissionsToAspNetProject.Startup))]
+namespace AddingInGroupPermissionsToAspNetProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
